4d8fd76abc227157324ba8c2bff0f2829df451ac
Linh Tran <72944083+lnhtrn@users.noreply.github.com>
2025-10-12 10:50:04 -0400

    Update aboutme.ts

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -1,5 +1,3 @@
-import headshot from "../images/linh_headshot.jpeg";
-
 export interface AboutMe {
   name: string;
   title: string;
@@ -27,13 +25,13 @@ export const aboutMe: AboutMe = {
   description:
     "I am a M.S. student in Human-Computer Interaction at University of Rochester, advised by Dr. Yukang Yan. My research explores how artificial intelligence can be integrated into human-centered systems to support diverse user needs in education, healthcare, and everyday life. I aim to develop AI-driven assistive technologies that enhance accessibility, empower underrepresented communities, and promote ethical, transparent design practices.\n\nMy research spans several interconnected directions, including applying generative AI for social good, designing AR/VR assistants for neurodivergent individuals, and creating AI-integrated tools to support healthcare professionals. I’m especially interested in understanding how users interact with intelligent systems and how technology can better adapt to their behaviors, challenges, and goals. Through these projects, I strive to bridge the gap between technical innovation and human empathy, ensuring that AI remains a tool for inclusion rather than exclusion.\n\nBefore starting my M.S. studies, I worked on projects in computer vision, psychology, and digital design that deepened my appreciation for interdisciplinary collaboration. My background in digital arts, creative writing, and programming helps me approach technology from both a technical and humanistic perspective. Through my research, I hope to build frameworks for trustworthy and accessible AI systems that make technology more equitable for everyone.",
   email: "ltran18@ur.rochester.edu",
-  imageUrl: headshot,
+  imageUrl: "/images/headshot.jpeg",
   googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
   githubUsername: "lnhtrn",
   linkedinUsername: "linh-ph-tran",
   twitterUsername: "nothing",
   blogUrl: "https://",
-  cvUrl: "/Linh_Tran_CV.pdf",
+  cvUrl: "/data/Linh_Tran_CV.pdf",
   institutionUrl: "https://www.rochester.edu/",
   // altName: "",
   // secretDescription: "I like dogs.",
