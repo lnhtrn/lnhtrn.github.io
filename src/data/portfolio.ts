ee849e45ded2b8105fd056854ed8671a3afe2727
Linh Tran <72944083+lnhtrn@users.noreply.github.com>
2025-10-12 11:40:51 -0400

    Update portfolio.ts

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -5,18 +5,89 @@ export interface Portfolio {
   imageUrl?: string;
   projectUrl?: string;
   codeUrl?: string;
+  award?: string;
 }
 
 export const portfolioData: Portfolio[] = [
-  // Example entry
+  // {
+  //   title: "LLMs as Sociologists: Leveraging AI/ML Contextual Knowledge for Social Oppression",
+  //   description: "Collaborated with epidemiologists to develop a LLM-based tool that analyzes global COVID-19 survey responses for indicators of systemic oppression and demographic bias.",
+  //   technologies: ["Python", "Transformers", "Large Language Model"],
+  //   projectUrl: "https://github.com/lnhtrn/Global-Ethnicity/",
+  //   imageUrl: "",
+  //   codeUrl: "https://github.com/lnhtrn/Global-Ethnicity/",
+  //   award: ""
+  // },
   {
-    title: "Causal Discovery Framework",
-    description:
-      "A framework for discovering causal relationships in high-dimensional time series data using state-of-the-art machine learning techniques.",
-    technologies: ["Python", "PyTorch", "React"],
-    projectUrl: "https://project-demo.com",
-    imageUrl:
-      "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    codeUrl: "https://github.com/username/project",
+    title: "AR Housework Assistant for Individuals with Executive Dysfunction",
+    description: "Enabled context-aware task prompting for individuals with ADHD by integrating real-time object recognition into an Meta Quest 3S to reduce executive dysfunction during household chores.",
+    technologies: ["C#", "Unity", "Sentis", "Python"],
+    projectUrl: "",
+    imageUrl: "",
+    codeUrl: "",
+    award: ""
   },
+  // {
+  //   title: "Investigation of Racial Bias in Vision-Language Assistants in Workplace Settings",
+  //   description: "Detect and analyze bias in Vision-Language Assistants (VLAs) used for hiring systems, showcasing that VLAs carried stereotypes and bias in them that can potentially harm candidate's chances of getting hired.",
+  //   technologies: ["Python", "Transformers", "Vision-Language Model"],
+  //   projectUrl: "https://github.com/lnhtrn/VLA-Workspace",
+  //   imageUrl: "",
+  //   codeUrl: "https://github.com/lnhtrn/VLA-Workspace",
+  //   award: "Poster Presentation at ACM NYCWiC '25"
+  // },
+  {
+    title: "URHungry - DandyHack",
+    description: "A web platform developed by Python and Taipy, enabling students to merge orders and collectively meet minimum price requirements for free shipping from popular grocery stores.",
+    technologies: ["Python", "Taipy", "CSS", "pandas"],
+    projectUrl: "https://github.com/javac0f/URHungry/",
+    imageUrl: "",
+    codeUrl: "https://github.com/javac0f/URHungry/",
+    award: ""
+  },
+  {
+    title: "DoubletDetection - GIDS Biomedical Data Science Hackathon",
+    description: "Implementation and optimization of DoubletDetection to predict doublet cells in single cell sequencing data, achieving a MCC score of 0.556.",
+    technologies: ["Python", "DoubletDetection", "numpy", "pandas", "matplotlib", "seaborn"],
+    projectUrl: "https://github.com/lnhtrn/Hackathon-Summer-2023",
+    imageUrl: "",
+    codeUrl: "https://github.com/lnhtrn/Hackathon-Summer-2023",
+    award: "First place Undergraduate Division"
+  },
+  {
+    title: "MRC with Increased Negative Samples",
+    description: "Analysis of DeBERTa v3's performance on SQuAD 2.0 dataset, with and without generated negative samples.",
+    technologies: ["Python", "Huggingface", "Transformer", "TensorFlow", "PyTorch", "numpy", "pandas", "matplotlib", "seaborn"],
+    projectUrl: "https://github.com/lnhtrn/SQuAD-DeBERTa-negative-samples",
+    imageUrl: "",
+    codeUrl: "https://github.com/lnhtrn/SQuAD-DeBERTa-negative-samples",
+    award: ""
+  },
+  {
+    title: "GoodSoup - DandyHack",
+    description: "A project that analyzes students’ allergies and food restrictions on University of Rochester's Dining Service’s daily recipes.",
+    technologies: ["Python", "API", "numpy", "pandas"],
+    projectUrl: "https://devpost.com/software/goodsoup",
+    imageUrl: "",
+    codeUrl: "https://devpost.com/software/goodsoup",
+    award: "Winner of Community Track"
+  },
+  {
+    title: "Rocky Road - CSC 214 Hackathon",
+    description: "An endless running game inspired by University of Rochester's mascot Rocky.",
+    technologies: ["Swift (iOS)", "Figma"],
+    projectUrl: "https://github.com/lnhtrn/Rocky-Road",
+    imageUrl: "",
+    codeUrl: "https://github.com/lnhtrn/Rocky-Road",
+    award: "Second place - Silver Joker prize"
+  },
+  // {
+  //   title: "Reversi",
+  //   description: "A Java implementation of the game Reversi/Othello. The computer's logic is determined by randomization or Minimax algorithm.",
+  //   technologies: ["Java"],
+  //   projectUrl: "https://github.com/lnhtrn/Reversi",
+  //   imageUrl: "",
+  //   codeUrl: "https://github.com/lnhtrn/Reversi",
+  //   award: ""
+  // }
 ];
