5e687f6992aa800c7047036308841484e1e9f771
lnhtrn <72944083+lnhtrn@users.noreply.github.com>
2026-01-03 14:19:52 -0500

    edit basic information

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -25,8 +25,8 @@ export const aboutMe: AboutMe = {
   // institution: "University of Rochester",
   // Note that links work in the description
   description:
-    "I am a M.S. student in Human-Computer Interaction at University of Rochester, advised by Dr. Yukang Yan. My research explores how artificial intelligence can be integrated into human-centered systems to support diverse user needs in education, healthcare, and everyday life. I aim to develop AI-driven assistive technologies that enhance accessibility, empower underrepresented communities, and promote ethical, transparent design practices.\n\nMy research spans several interconnected directions, including applying generative AI for social good, designing AR/VR assistants for neurodivergent individuals, and creating AI-integrated tools to support healthcare professionals. I’m especially interested in understanding how users interact with intelligent systems and how technology can better adapt to their behaviors, challenges, and goals. Through these projects, I strive to bridge the gap between technical innovation and human empathy, ensuring that AI remains a tool for inclusion rather than exclusion.\n\nBefore starting my M.S. studies, I worked on projects in computer vision, psychology, and digital design that deepened my appreciation for interdisciplinary collaboration. My background in digital arts, creative writing, and programming helps me approach technology from both a technical and humanistic perspective. Through my research, I hope to build frameworks for trustworthy and accessible AI systems that make technology more equitable for everyone.",
-  email: "ltran18@ur.rochester.edu",
+    "I am an incoming Ph.D. student at the New Jersey Institute of Technology, and I will join Dr. Sooyeon Lee's lab to work on Human-Computer Interaction projects for accessibility for the blind and low-vision community. \n\nPreviously, before joining NJIT, I was a M.S. student in Human-Computer Interaction at the University of Rochester, advised by Dr. Yukang Yan. My research explores how artificial intelligence can be integrated into human-centered systems to support diverse user needs in education, healthcare, and everyday life. I aim to develop AI-driven assistive technologies that enhance accessibility, empower underrepresented communities, and promote ethical, transparent design practices.\n\nMy research spans several interconnected directions, including applying generative AI for social good, designing AR/VR assistants for neurodivergent individuals, and creating AI-integrated tools to support healthcare professionals. I’m especially interested in understanding how users interact with intelligent systems and how technology can better adapt to their behaviors, challenges, and goals. Through these projects, I strive to bridge the gap between technical innovation and human empathy, ensuring that AI remains a tool for inclusion rather than exclusion.\n\nBefore starting my M.S. studies, I worked on projects in computer vision, psychology, and digital design that deepened my appreciation for interdisciplinary collaboration. My background in digital arts, creative writing, and programming helps me approach technology from both a technical and humanistic perspective. Through my research, I hope to build frameworks for trustworthy and accessible AI systems that make technology more equitable for everyone.",
+  email: "lt297@njit.edu",
   imageUrl: "/images/headshot.jpeg",
   googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
   githubUsername: "lnhtrn",
@@ -34,7 +34,7 @@ export const aboutMe: AboutMe = {
   twitterUsername: "nothing",
   blogUrl: "https://",
   cvUrl: "/data/Tran_Linh_CV.pdf",
-  institutionUrl: "https://www.rochester.edu/",
+  institutionUrl: "https://www.njit.edu/",
   // altName: "",
   // secretDescription: "I like dogs.",
 };
